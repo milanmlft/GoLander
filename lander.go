37ff8cf79822599d7656f903973933460065b361
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-03-29 23:31:25 +0000

    Add the lander

---
+++ lander.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"image"
+	_ "image/png"
+	"log"
+	"os"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const landerPng = "img/mainchar.png"
+
+type Lander struct {
+	x, y   float64 // Position of the lander
+	vx, vy float64 // Velocity of the lander
+	fuel   float64 // Amount of fuel left
+	angle  float64 // Rotation angle of the lander (degrees)
+	img    *ebiten.Image
+}
+
+func NewLander() Lander {
+	img, err := loadImageFromFile(landerPng)
+	if err != nil {
+		log.Fatalf("failed to load image: %v", err)
+	}
+
+	landerImage := ebiten.NewImageFromImage(img)
+	return Lander{
+		x:    screenWidth / 2,
+		y:    50,
+		fuel: 100,
+		img:  landerImage,
+	}
+}
+
+func loadImageFromFile(filepath string) (image.Image, error) {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	img, _, err := image.Decode(f)
+	return img, err
+}
